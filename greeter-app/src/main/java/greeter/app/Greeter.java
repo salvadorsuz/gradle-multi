656223e440f3e0bcd0cd7caafcd62867ee70f786
salvadorsuz <salvadorsuz@yahoo.es>
2020-05-27 18:43:48 +0200

    refactor

--- greeter-app/src/main/java/greeter/app/Greeter.java
+++ greeter-app/src/main/java/greeter/app/Greeter.java
@@ -8,6 +8,7 @@ import greeter.GreetingFormatter;
 public class Greeter {
 
     public static void main(String[] args) {
+
         final String output = GreetingFormatter.greeting(args[0]);
         System.out.println(output);
     }
